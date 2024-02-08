e7c6883ff1098ad04fa64df1689a32a13e233a1b
nuratann <kadyrov.atan@gmail.com>
2024-02-08 14:34:12 +0600

    initial commit for backend

---
+++ backend/MSNBackend/src/main/java/com/fifthteam/msnbackend/MsnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fifthteam.msnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsnBackendApplication.class, args);
+    }
+
+}
---
+++ backend/MSNBackend/src/test/java/com/fifthteam/msnbackend/MsnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifthteam.msnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
